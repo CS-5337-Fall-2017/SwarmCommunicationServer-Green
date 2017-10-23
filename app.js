9c60c456833ef3785bc08e6d6f8f3236a71dbf87
luisfisherjr <luisfisherjr@gmail.com>
2017-10-22 20:05:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var enums = require('./enums');
 //JSON obj of a maptile. must be in ALL CAPS, as in enum value
 //     {	
 //		   "scanned": "0000" // binary represents what sensors have scanned the tile from left to right is Chemical, Radar, Radiation, Spectral
-//		   "rover": "ROVER_06", // rover locations with rover name any rover that connects to this RPC or ""
-//         "f": 7,
+//		   "rover": "ROVER_06", // rover locations with rover name any rover that connects to this server or ""
 //         "science": "CRYSTAL",  // , ORGANIC, MINERAL, ARTIFACT, CRYSTAL, NONE
 //		   "x": 12,
 //         "y": 14,
